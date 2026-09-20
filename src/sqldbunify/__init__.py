3f2bb474700f93a50d96480db5b94092a0278b87
tsy-pavel <tsyganovpavel@yandex.ru>
2026-09-21 00:42:50 +0300

    feat: add shared types and its imports

--- src/sqldbunify/__init__.py
+++ src/sqldbunify/__init__.py
@@ -1,2 +1,24 @@
 # SPDX-FileCopyrightText: 2026 Tsyganov Pavel
 # SPDX-License-Identifier: MIT
+
+from .types import (
+    FilterGroup,
+    FilterTuple,
+    FilterType,
+    JoinType,
+    LogicType,
+    OperatorType,
+    ReturnMode,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "FilterType",
+    "FilterTuple",
+    "FilterGroup",
+    "ReturnMode",
+    "JoinType",
+    "OperatorType",
+    "LogicType",
+]
--- src/sqldbunify/types.py
+++ src/sqldbunify/types.py
@@ -1,2 +1,31 @@
 # SPDX-FileCopyrightText: 2026 Tsyganov Pavel
 # SPDX-License-Identifier: MIT
+
+from typing import Any, Literal, Optional, TypeVar, Union
+
+OperatorType = Literal['==', '!=', '>', '<', '>=', '<=', 'in', 'not_in', 'between']
+
+FilterTuple = tuple[str, OperatorType, Any]
+FilterGroup = list[Union[str, FilterTuple, "FilterGroup"]]
+FilterType = Optional[Union[dict, list[FilterTuple], FilterGroup]]
+
+ReturnMode = Literal['list', 'lists', 'tuple', 'tuples', 'dict', 'single_item', 'default']
+
+JoinType = Literal['inner', 'outer']
+LogicType = Literal['and', 'or']
+
+ModelType = TypeVar('ModelType')
+FilterValue = Any
+
+
+__all__ = [
+    "OperatorType",
+    "FilterTuple",
+    "FilterGroup",
+    "FilterType",
+    "ReturnMode",
+    "JoinType",
+    "LogicType",
+    "ModelType",
+    "FilterValue",
+]
